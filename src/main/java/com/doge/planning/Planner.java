d3b928b3f1df22d69b7a693eb8a15dbd222705e7
Petr Mánek <petr.manek@seznam.cz>
2014-08-11 00:02:31 +0200

    First build.

---
+++ src/main/java/com/doge/planning/Planner.java
@@ -0,0 +1,10 @@
+package com.doge.planning;
+
+/**
+ * Created by petrmanek on 10.08.14.
+ */
+public class Planner {
+    public static void main(String[] args) {
+        System.out.println("such plan. wow.");
+    }
+}
